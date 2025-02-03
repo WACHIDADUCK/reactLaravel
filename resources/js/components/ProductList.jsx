76e47cd29144d7815caecc4b428da973d8d43f6a
WACHIDADUCK <captainvantruc@gmail.com>
2025-02-03 18:12:40 +0000

    A33. Autenticación con React y Laravel Breeze

--- resources/js/components/ProductList.jsx
+++ resources/js/components/ProductList.jsx
@@ -2,25 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-
-export default function ProductList() {
+const Main = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
-    const [paginaActual, setPaginaActual] = useState();
-    const [productsPage, setProductsPage] = useState();
-
-
-
 
     useEffect(() => {
         // Función para obtener los productos desde el endpoint
         const fetchProducts = async () => {
             try {
-                const response = await axios.get('/api/products');
-                setProducts(response.data); // Asumiendo que los datos están en response.data.data
+                const response = await axios.get('/api/products', {withCredentials: true});
+                setProducts(response.data.data); // Asumiendo que los datos están en response.data.data
                 setLoading(false);
+                if (response.redirected) {
+                    window.location.href = response.url;
+                    return;
+                    }
             } catch (err) {
                 setError(err.message);
                 setLoading(false);
@@ -30,38 +27,26 @@ export default function ProductList() {
         fetchProducts();
     }, []); // El array vacío como segundo argumento asegura que esto se ejecute solo una vez
 
-
-    useEffect(() => {
-        console.log(products);
-    }, [paginaActual])
-
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
     return (
 
-        <div>
-            {
-                products.meta.links.map((link, index) => (
-                    <a key={index} href={link.url}>{link.label}</a>
-                ))
-            }
+        <div className="card">
             <h1>Product List</h1>
             <ul>
-                {products.data.map(product => (
-                    <div>
-                        <img src={product.image} />
-
-                        <li key={product.id}>
-                            <h2>{product.name}</h2>
-                            <p>{product.description}</p>
-                            <p>Price: ${product.price}</p>
-                            <p>Quantity: {product.quantity}</p>
-                        </li>
-                    </div>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                        <p>Quantity: {product.quantity}</p>
+                    </li>
                 ))}
             </ul>
         </div>
 
     );
 };
+
+export default Main;
